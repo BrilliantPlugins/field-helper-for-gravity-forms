ba57fc6729e2de90b0018473b8ce0eb499d6d843
Andrew Minion <andrew.minion@luminfire.com>
2021-04-21 14:26:28 -0500

    consolidate duplicate code

--- class-gf-field-helper-common.php
+++ class-gf-field-helper-common.php
@@ -25,7 +25,16 @@ class GF_Field_Helper_Common {
 	 *
 	 * @var array $friendly_labels
 	 */
-	protected $friendly_labels = array();
+	protected static $friendly_labels = array();
+
+	/**
+	 * Checkbox fields to coalasce.
+	 *
+	 * @since 1.0.3.0
+	 *
+	 * @var array $checkbox_fields
+	 */
+	protected static $checkbox_fields = array();
 
 	/**
 	 * Convert field ID with period to underscore.
@@ -49,21 +58,32 @@ class GF_Field_Helper_Common {
 	 *
 	 * @return array        Modified entry object.
 	 */
-	public function replace_field_names( $result ) {
-		$labels = $this->get_form_friendly_labels( $result['form_id'] );
-
-		// Bail out if no labels are set.
-		if ( empty( $labels ) ) {
-			return $result;
-		}
+	public static function replace_field_names( $result ) {
+		$labels = self::get_form_friendly_labels( $result['form_id'] );
 
 		$fields = array();
 
 		foreach ( $result as $key => $value ) {
 			$sanitized_key = self::convert_field_id( $key );
 
+			if ( empty( $labels ) ) {
+				$fields['error'] = array(
+					'code'    => 500,
+					'message' => 'Friendly field names are not set. Please visit your form settings to set them.',
+				);
+			}
+
 			if ( in_array( $sanitized_key, array_flip( $labels ), false ) ) { // phpcs:ignore WordPress.PHP.StrictInArray -- since GF uses both integer and string field keys.
-				$fields[ $labels[ $sanitized_key ] ] = $value;
+
+				if ( in_array( absint( $sanitized_key ), self::$checkbox_fields, true ) ) {
+					// Checkbox.
+					if ( ! empty( $value ) ) {
+						$fields[ $labels[ absint( $sanitized_key ) ] ][] = $value;
+					}
+				} else {
+					// Others.
+					$fields[ $labels[ $sanitized_key ] ] = $value;
+				}
 			}
 
 			// Unset only field keys (strings will convert to 0, floats to integers).
@@ -84,21 +104,39 @@ class GF_Field_Helper_Common {
 	 *
 	 * @param int $form_id Form ID.
 	 *
-	 * @return array|false Human-friendly form labels or false if not set.
+	 * @return array Human-friendly form labels or false if not set.
 	 */
-	public function get_form_friendly_labels( $form_id ) {
-		if ( ! isset( $this->friendly_labels[ $form_id ] ) ) {
+	public static function get_form_friendly_labels( $form_id ) {
+		if ( ! isset( self::$friendly_labels[ $form_id ] ) ) {
 			$form = GFAPI::get_form( $form_id );
 
-			// Bail out if no labels are set.
 			if ( ! array_key_exists( GF_FIELD_HELPER_SLUG, $form ) ) {
-				return false;
+				return array();
 			}
 
-			$this->friendly_labels[ $form_id ] = array_filter( $form[ GF_FIELD_HELPER_SLUG ] );
+			$fields = array_filter( $form[ GF_FIELD_HELPER_SLUG ] );
+
+			foreach ( $form['fields'] as $field ) {
+				if ( 'checkbox' === $field['type'] && array_key_exists( $field['id'] . '-checkbox-return', $fields ) && 'combined' === $fields[ $field['id'] . '-checkbox-return' ] ) {
+
+					// Unset the choices.
+					foreach ( $field['inputs'] as $input_key => $input_id ) {
+						$input_id = self::convert_field_id( $input_id['id'] );
+						unset( $fields[ $input_id ] );
+					}
+
+					// Set array of checkbox fields.
+					self::$checkbox_fields[ $field['id'] ] = $field['id'];
+				}
+
+				// Unset the Field Helper setting for checkboxes.
+				unset( $fields[ $field['id'] . '-checkbox-return' ] );
+			}
+
+			self::$friendly_labels[ $form_id ] = $fields;
 		}
 
-		return $this->friendly_labels[ $form_id ];
+		return self::$friendly_labels[ $form_id ];
 	}
 
 }
--- class-gf-field-helper-endpoint.php
+++ class-gf-field-helper-endpoint.php
@@ -37,24 +37,6 @@ class GF_Field_Helper_Endpoint extends GF_REST_Entries_Controller {
 	public $rest_base = 'json';
 
 	/**
-	 * Our cache of human-friendly labels.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @var array $friendly_labels
-	 */
-	protected $friendly_labels = array();
-
-	/**
-	 * Checkbox fields to coalasce.
-	 *
-	 * @since 1.0.3.0
-	 *
-	 * @var array $checkbox_fields
-	 */
-	protected $checkbox_fields = array();
-
-	/**
 	 * Register our REST endpoint.
 	 *
 	 * @since 1.0.0
@@ -172,104 +154,13 @@ class GF_Field_Helper_Endpoint extends GF_REST_Entries_Controller {
 		$results = $response->get_data();
 
 		if ( $single ) {
-			$results = $this->replace_field_names( $results );
+			$results = GF_Field_Helper_Common::replace_field_names( $results );
 		} else {
 			foreach ( $results['entries'] as $key => $result ) {
-				$results['entries'][ $key ] = $this->replace_field_names( $result );
+				$results['entries'][ $key ] = GF_Field_Helper_Common::replace_field_names( $result );
 			}
 		}
 
 		return new WP_REST_Response( $results, $response->get_status() );
 	}
-
-	/**
-	 * Replace field IDs with human-friendly names.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @param array $result Original entry object.
-	 *
-	 * @return array        Modified entry object.
-	 */
-	public function replace_field_names( $result ) {
-		$labels = $this->get_form_friendly_labels( $result['form_id'] );
-
-		$fields = array();
-
-		foreach ( $result as $key => $value ) {
-			$sanitized_key = GF_Field_Helper_Common::convert_field_id( $key );
-
-			if ( empty( $labels ) ) {
-				$fields['error'] = array(
-					'code'    => 500,
-					'message' => 'Friendly field names are not set. Please visit your form settings to set them.',
-				);
-			}
-
-			if ( in_array( $sanitized_key, array_flip( $labels ), false ) ) { // phpcs:ignore WordPress.PHP.StrictInArray -- since GF uses both integer and string field keys.
-
-				if ( in_array( absint( $sanitized_key ), $this->checkbox_fields, true ) ) {
-					// Checkbox.
-					if ( ! empty( $value ) ) {
-						$fields[ $labels[ absint( $sanitized_key ) ] ][] = $value;
-					}
-				} else {
-					// Others.
-					$fields[ $labels[ $sanitized_key ] ] = $value;
-				}
-			}
-
-			// Unset only field keys (strings will convert to 0, floats to integers).
-			if ( 0 !== absint( $key ) ) {
-				unset( $result[ $key ] );
-			}
-		}
-
-		$result['fields'] = $fields;
-
-		return $result;
-	}
-
-	/**
-	 * Get friendly labels for the given form.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @param int $form_id Form ID.
-	 *
-	 * @return array       Human-friendly form labels.
-	 */
-	public function get_form_friendly_labels( $form_id ) {
-		if ( ! isset( $this->friendly_labels[ $form_id ] ) ) {
-			$form = GFAPI::get_form( $form_id );
-
-			if ( ! array_key_exists( GF_FIELD_HELPER_SLUG, $form ) ) {
-				return array();
-			}
-
-			$fields = array_filter( $form[ GF_FIELD_HELPER_SLUG ] );
-
-			foreach ( $form['fields'] as $field ) {
-				if ( 'checkbox' === $field['type'] && array_key_exists( $field['id'] . '-checkbox-return', $fields ) && 'combined' === $fields[ $field['id'] . '-checkbox-return' ] ) {
-
-					// Unset the choices.
-					foreach ( $field['inputs'] as $input_key => $input_id ) {
-						$input_id = GF_Field_Helper_Common::convert_field_id( $input_id['id'] );
-						unset( $fields[ $input_id ] );
-					}
-
-					// Set array of checkbox fields.
-					$this->checkbox_fields[ $field['id'] ] = $field['id'];
-				}
-
-				// Unset the Field Helper setting for checkboxes.
-				unset( $fields[ $field['id'] . '-checkbox-return' ] );
-			}
-
-			$this->friendly_labels[ $form_id ] = $fields;
-		}
-
-		return $this->friendly_labels[ $form_id ];
-	}
-
 }
